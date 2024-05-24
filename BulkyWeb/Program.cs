9cf6f57423ba7562f2df7b591f298887de9b0f70
António Caldeira <antoniocaldeira@yahoo.com>
2024-05-24 22:49:52 +0100

    All categories List, new bootstrap

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
