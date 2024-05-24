c4116ec8ac73142d8d237beb756a9ff95abed525
António Caldeira <antoniocaldeira@yahoo.com>
2024-05-24 21:31:16 +0100

    Add DbSet and migration

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
@@ -6,7 +7,9 @@ namespace BulkyWeb.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
+            
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
